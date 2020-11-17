0ac3e90a8f5a495896e02e47bbf6ceb94520c39b
Apple <apple@apples-air.home>
2020-11-17 12:24:44 -0800

    new js and index changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
-jQuery (function() {
+jQuery(function() {
 
 	let my_switch = false;
 
-$("#mybutton").click(function(){
+$('#mybutton').click(function () {
 
    if ( my_switch == false) {
    my_switch = true;
-   $('#my_image').attr ('src', '../images/yellow_chair.png');
 
-} else {
-	my_switch = false;
-	$('#my_image').attr ('src', '../images/leather_chair.png');
+	 $(this).html('velvet chair');
+   $('#image').attr('src', 'images/leather_chair.png');
+
+	 } else {
+
+	   my_switch = false;
+
+		 $(this).html('leather chair');
+	   $('#image').attr('src', 'images/yellow_chair.png');
 }
 
 
