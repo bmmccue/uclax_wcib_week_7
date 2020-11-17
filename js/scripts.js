912e97d126147394c7a81e1fa7f42f7903de39bf
Apple <apple@apples-air.home>
2020-11-16 23:39:48 -0800

    interior scene changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
 jQuery(function() {
 
-jQuery('#my_button').click(function () {
-	conaole.log('my_button CLICKED!');
-	$('.square').addClass('red');
-});
+let my_switch = false;
+
+$('#my_button').click(function () {
+
+	//if ( my_switch == false) {
+		//my_switch = true;
+	  //$('.square.alt').addClass('red');
+//}   else {
+	 // my_switch = false;
+	//$('.square.alt').removeClass('red');
+//}
+
+
+if ( $('.square.alt').hasClass('red') ) {
+	$('.square.alt').removeClass('red');
+}   else {
+$('.square.alt').addClass('red');
+}
+
+  });
 
 });
