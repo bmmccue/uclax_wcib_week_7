386d53150b1f9cc751b2c1d3a40e56757d9650d8
Apple <apple@apples-air.home>
2020-11-20 11:05:48 -0800

    new js changes

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ $('#mybutton').click(function () {
    my_switch = true;
 
 	 $(this).html('velvet chair');
-   $('#image').attr('src', '../images/leather_chair.png');
+   $('#image').attr('src', 'images/leather_chair.png');
 
 	 } else {
 
 	   my_switch = false;
 
 		 $(this).html('leather chair');
-	   $('#image').attr('src', '../images/yellow_chair.png');
+	   $('#image').attr('src', 'images/yellow_chair.png');
 }
 
 
